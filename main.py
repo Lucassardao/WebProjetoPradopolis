6654f28abea5506f2f2bd2f92cddaad1d814c793
Ricardo Medeiros <ricardo.h.mederios@gmail.com>
2021-11-14 17:56:41 -0300

    Criado base para o site, pasta de estilo e imagens

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 #criar a instancia do flask
-app = Flask(__name__)
+app = Flask(__name__,template_folder='static/templates')
 
 #criar a rota
 @app.route('/')
