d311530e93fb2ac4b035dfa67147ed5c00118986
Ricardo Medeiros <ricardo.h.mederios@gmail.com>
2021-11-19 22:07:02 -0300

    Criado estilo da pg de registro, arrumado o caminho para imagens e estilo da pg de registro

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
 #criar a instancia do flask
-app = Flask(__name__,template_folder='static/templates',static_url_path='/static')
+app = Flask(__name__)
 
 #criar a rota
-@app.route('/')
+@app.route('/index.html')
 
 def index():
     return render_template("index.html")
